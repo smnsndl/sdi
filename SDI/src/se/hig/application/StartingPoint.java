82a4bae5be5766ea2be9a377dcc1ca5c799384ce
Fredrik Andersson <nit10fan@student.hig.com>
2014-10-17 20:32:02 +0200

    Test 2, lade till ännu en sketen textutskrift i startingpoint.java

--- SDI/src/se/hig/application/StartingPoint.java
+++ SDI/src/se/hig/application/StartingPoint.java
@@ -15,6 +15,11 @@ public class StartingPoint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		
+		System.out.println("GIT test 2 commit shit!");
+		
+		
+		
 	}
 
 }
