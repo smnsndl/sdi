79d56a3ec63354e960263ecf7b5453b33dfa496e
Fredrik Andersson <nit10fan@student.hig.com>
2014-10-17 20:00:17 +0200

    StartingPoint.java tillagd. Två nya package tillagd.
    Test Git

---
+++ SDI/src/se/hig/application/StartingPoint.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package se.hig.application;
+
+/**
+ * @author fredrik
+ *
+ */
+public class StartingPoint {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
